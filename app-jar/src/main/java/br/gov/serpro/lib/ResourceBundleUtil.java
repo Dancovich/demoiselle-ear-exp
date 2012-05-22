36606b73781035eef28de8ef4c7f5d8d7c581b86
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2012-05-22 07:57:11 -0300

    Convertendo o projeto a lib app-jar em um projeto CDI (por conter o /META-INF/beans.xml) e convertendo a classe utilitária que fabrica ResourceBundle em um CDI Producer para possibilitar a injeção de ResourceBundle.

--- app-jar/src/main/java/br/gov/serpro/lib/ResourceBundleUtil.java
+++ app-jar/src/main/java/br/gov/serpro/lib/ResourceBundleUtil.java
@@ -2,8 +2,13 @@ package br.gov.serpro.lib;
 
 import java.util.ResourceBundle;
 
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+
 public class ResourceBundleUtil {
 
+	@Produces
+	@Default
 	public static ResourceBundle getResourceBundle() {
 		return ResourceBundle.getBundle("messages");
 	}
